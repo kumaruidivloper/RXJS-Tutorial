607ef1abc878835e5a0d5de378d68cb0a1a99a86
kumarshan <kumaruidivloper@gmail.com>
2019-07-19 13:26:35 +0530

    working-with-Async-Pipe

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,35 +1,46 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+// import { Subscription } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products: Product[] = [];
-  sub: Subscription;
+  // previous assignment with-out async pipe
+  // products: Product[] = [];
+
+  // new assignment with async pipe
+  products$: Observable<Product[]>;
+
+  // un-used removed
+  // sub: Subscription;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe(
-        products => this.products = products,
-        error => this.errorMessage = error
-      );
+    this.products$ = this.productService.getProducts();
+    // Below code Not reqieurd if we do Async Pipe
+
+    // this.sub = this.productService.getProducts()
+    //   .subscribe(
+    //     products => this.products = products,
+    //     error => this.errorMessage = error
+    //   );
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  // Below code Not reqieurd if we do Async Pipe
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe();
+  // }
 
   onAdd(): void {
     console.log('Not yet implemented');
