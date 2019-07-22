7ccbf0f0a1906f9fc8820245c825977ac46ff7f6
kumarshan <kumaruidivloper@gmail.com>
2019-07-22 15:43:37 +0530

    Ancillary-Stream-Display-Selected-Item-In-Page-Title

--- src/app/products/product-list-alt/product-detail.component.ts
+++ src/app/products/product-list-alt/product-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 import { ProductService } from '../product.service';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { EMPTY, Subject } from 'rxjs';
+import { Product } from '../product';
 
 @Component({
   selector: 'pm-product-detail',
@@ -10,7 +11,7 @@ import { EMPTY, Subject } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent {
-  pageTitle = 'Product Detail';
+  // pageTitle = 'Product Detail';
   private errorMessageSubject = new Subject<string>();
   errorMessage = '';
 
@@ -21,6 +22,11 @@ export class ProductDetailComponent {
         return EMPTY;
       })
     );
+
+  pageTitle$ = this.product$
+      .pipe(
+        map((p: Product) => p ? `Product Detail for: ${p.productName}` : null )
+      );
 
   productSuppliers$ = this.productService.selectedProductSuppliers$
   .pipe(
